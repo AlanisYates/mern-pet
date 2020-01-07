edd91c0483f81cf4896ce9502b480697e0f56611
AlanisYates <yatesalanis@gmail.com>
2020-01-06 19:30:37 -0500

    changing layout

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
 import React from "react";
 // React Router is used to ctalk to the backend and the routes created.
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 // Imported bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 // // Importing the Components
-// import Navbar from "./components/Navbar.component";
-// import ExerciseList from "./components/exercises-list.component";
-// import EditExercise from "./components/edit-exercises.component";
-// import CreateExercise from "./components/create-exercises.component";
-// import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <h1>This is working</h1>
-        {/* All the components are below */}
-        <Navbar />
-        <br />
-        {/* React router help us route the url path and data to that path */}
-        <Route path="/" exact component={ExerciseList} />
-        <Route path="/edit/:id" component={EditExercise} />
-        <Route path="/create" component={CreateExercise} />
-        <Route path="/user" component={CreateUser} />
-      </div>
-    </Router>
+    <div className="App">
+      <h1>Hello this work?</h1>
+    </div>
   );
 }
 
